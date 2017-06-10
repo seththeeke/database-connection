48a91c585748344a323beb20cc9699eff3afd020
Seth Theeke <Seth.P.Theeke@leidos.com>
2017-06-10 13:36:25 -0400

    Added query to Database Interface

--- src/main/IDatabaseConnection.java
+++ src/main/IDatabaseConnection.java
@@ -3,9 +3,11 @@ package main;
 import java.sql.SQLException;
 import java.util.Map;
 
-import mysql.ColumnType;
+import mysql.MySqlColumnType;
 
 public interface IDatabaseConnection {
 
-	public void createTable(Map<String, ColumnType> tableColumns) throws SQLException;
+	public void createTable(final String tableName, Map<String, MySqlColumnType> tableColumns) throws SQLException;
+		
+	public void query(String query) throws SQLException;
 }
