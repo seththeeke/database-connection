70433f93eaa34e3f25792c6c1864e7269a9dcb10
Seth Theeke <Seth.P.Theeke@leidos.com>
2017-06-10 20:29:19 -0400

    Added DatabaseObject

--- src/main/DatabaseObject.java
+++ src/main/DatabaseObject.java
@@ -1,5 +1,28 @@
 package main;
 
-public class DatabaseObject {
+import java.util.Map;
 
+public class DatabaseObject {
+	
+	private Map<String, Object> rowProperties;
+	
+	public DatabaseObject(Map<String, Object> rowProperties){
+		this.rowProperties = rowProperties;
+	}
+	
+	public Object getProperty(String column){
+		return this.rowProperties.get(column);
+	}
+	
+	public Map<String, Object> getProperties(){
+		return this.rowProperties;
+	}
+	
+	public String toString(){
+		StringBuilder builder = new StringBuilder();
+		for (String key: this.rowProperties.keySet()){
+			builder.append(key + " ==> " + this.rowProperties.get(key)).append("\n");
+		}
+		return builder.toString();
+	}
 }
