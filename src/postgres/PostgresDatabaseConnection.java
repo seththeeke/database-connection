e8360ab45471af6583ba4ff89e6fbe305c3e3056
Seth Theeke <Seth.P.Theeke@leidos.com>
2017-06-10 20:32:27 -0400

    Added Postgres driver and configuration

--- src/postgres/PostgresDatabaseConnection.java
+++ src/postgres/PostgresDatabaseConnection.java
@@ -1,30 +1,50 @@
 package postgres;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Map;
 
 import main.DatabaseException;
-import main.DatabaseObject;
 import main.IDatabaseConnection;
+import main.DatabaseObject;
 import mysql.MySqlColumnType;
 
 public class PostgresDatabaseConnection implements IDatabaseConnection{
-
-	@Override
-	public void createTable(String tableName, Map<String, MySqlColumnType> tableColumns) throws DatabaseException {
-		// TODO Auto-generated method stub
-		
+	
+	private String databasePath;
+	private String databaseUsername;
+	private String databasePassword;
+	
+	public PostgresDatabaseConnection(final String databasePath, final String databaseUsername, final String databasePassword) {
+		this.databasePath = databasePath;
+		this.databaseUsername = databaseUsername;
+		this.databasePassword = databasePassword;
 	}
 
 	@Override
-	public void query(String query) throws DatabaseException {
-		// TODO Auto-generated method stub
-		
+	public void createTable(String tableName, Map<String, MySqlColumnType> tableColumns) throws DatabaseException {
+		Connection databaseConnection = connectToDatabase();
+	}
+
+	@Override
+	public List<DatabaseObject> query(String query) throws DatabaseException {
+		Connection databaseConnection = connectToDatabase();
+		return null;
 	}
 
 	@Override
 	public void persistObject(String tableName, DatabaseObject object) throws DatabaseException {
-		// TODO Auto-generated method stub
-		
+		Connection databaseConnection = connectToDatabase();
+	}
+	
+	private Connection connectToDatabase() throws DatabaseException {
+		try {
+			return DriverManager.getConnection(this.databasePath, this.databaseUsername, this.databasePassword);
+		} catch (SQLException e) {
+			throw new DatabaseException(e);
+		}
 	}
 
 }
