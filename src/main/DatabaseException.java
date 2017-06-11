dbd1c75804b36da47d52c2a9d877906c2ecb6095
Seth Theeke <Seth.P.Theeke@leidos.com>
2017-06-10 20:28:35 -0400

    Added stack trace to database exception

--- src/main/DatabaseException.java
+++ src/main/DatabaseException.java
@@ -3,7 +3,7 @@ package main;
 public class DatabaseException extends Throwable{
 	
 	public DatabaseException(Exception e) {
-		// TODO Auto-generated constructor stub
+		e.printStackTrace();
 	}
 
 }
